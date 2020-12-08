ef92e014244147c09fb83417f957a8985f5712e2
DevExpressExampleBot <fakeEmail>
2020-12-08 19:35:51 +0300

    Source auto update [skip ci]

---
+++ VB/JS/dxSample_E4738/dxSample_PhoneJSApp-2/PhoneJSApp/Views/js/home.js
@@ -0,0 +1,47 @@
+﻿PhoneJSApp.home = function () {
+    var viewModel = {
+        linearGaugeOptions: {
+            scale: ko.observable({
+                startValue: 0,
+                endValue: 100,
+                majorTick: {
+                    showCalculatedTicks: false,
+                    customTickValues: [0, 15, 60, 100]
+                }
+            }),
+            rangeContainer: {
+                backgroundColor: 'none',
+                ranges: [
+                {
+                    startValue: 0,
+                    endValue: 15,
+                    color: '#A6C567'
+                },
+                {
+                    startValue: 15,
+                    endValue: 60,
+                    color: '#FCBB69'
+                },
+                {
+                    startValue: 60,
+                    endValue: 100,
+                    color: '#E19094'
+                }
+                ]
+            },
+            title: ko.observable({
+                text: "Title",
+            }),
+            needles: [{ value: 42 }],
+            markers: [{ value: 32 }],
+            rangeBars: [{ value: 32 }]
+        }
+    };
+
+    viewModel.changeText = function () {
+        viewModel.linearGaugeOptions.title({ text: 'Title has been changed', position: 'top-left' });
+        viewModel.linearGaugeOptions.scale({ majorTick: {color: 'Red' } });
+    };
+
+    return viewModel;
+}
---
+++ VB/JS/dxSample_E4738/dxSample_PhoneJSApp-2/PhoneJSApp/index.js
@@ -0,0 +1,15 @@
+﻿window.PhoneJSApp = {}; //window.AppNamespace || {};
+
+$(function () {
+    var html = DevExpress.framework.html;
+    html.layoutControllers.default = new html.DefaultLayoutController;
+    PhoneJSApp.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: PhoneJSApp,
+        defaultLayout: 'default'
+     });
+
+    PhoneJSApp.app.router.register(":view/:name", { view: "home", name: '' });
+    PhoneJSApp.app.navigate();
+
+    var deviceInfo = PhoneJSApp.app.device;
+});
